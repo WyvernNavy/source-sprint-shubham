4bea08321c2db6a49b686f3f8c30cf0fc8bcd1db
WyvernNavy <145483744+WyvernNavy@users.noreply.github.com>
2023-09-23 14:47:50 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+x= eval(input("Enter the first number:"))
+y= eval(input("Enter the second number:"))
+
+sum = x+y
+diff = x-y
+
+print("The sum is ",sum)
+print("The difference is ",diff)
+print("This is a calculator")
